17a7c9717e96896493b01cf5c5bd871ff3f17629
beuoon <beuoon@naver.com>
2024-10-22 23:51:48 +0900

    Fix: 유튜브 뮤직 화면 변경 대응
    - 믹스 삭제
    - 플레이리스트 조회 방식 변경
    - 탭 관련 코드 제거
    - 볼륨 설정 방식 변경

--- extension/ym_control.js
+++ extension/ym_control.js
@@ -1,87 +1,20 @@
-
-var TAB_HOME = 0;
-var TAB_EXPLORE = 1;
-var TAB_STORAGE = 2;
-var TAB_SEARCH = 3;
-
-function sleep(ms) {
-    return new Promise(resolve=>setTimeout(resolve, ms));
-}
-
 function getMusicTitle() {
     return document.getElementsByClassName('title style-scope ytmusic-player-bar')[0].getAttribute('title');
 }
 function checkIsPlaying() {
     return getMusicTitle() != "";
 }
-function checkIsCurrentTab(index) {
-    var status = document.getElementsByClassName('style-scope ytmusic-pivot-bar-renderer')[index].getAttribute('aria-selected');
-    return status == "true";
-}
-function moveTab(index) {
-    return new Promise(async function(resolve) {
-        document.getElementsByClassName('style-scope ytmusic-pivot-bar-renderer')[index].click();
+function dispatchEvenetToVolumeSlider(keyEvent) {
+    let slider = document.getElementById('volume-slider');
 
-        let MAX_LOOP_CNT = 100;
-        let bMoved = false;
-
-        for (let i = 0; i < MAX_LOOP_CNT; i++) {
-            if (checkIsCurrentTab(index)) {
-                bMoved = true;
-                break;
-            }
-            await sleep(100);
-        }
-
-        resolve(bMoved);
-    });
-}
-
-function getVolume() {
-    return parseInt(document.getElementById('volume-slider').getAttribute('value'));
-}
-function setVolume(size) {
-    if (size < 0) size = 0;
-    if (size > 100) size = 100;
-
-    document.getElementById('volume-slider').setAttribute('value', size);
+    for (let i = 0; i < 5; i++) {
+        slider.dispatchEvent(keyEvent);
+    }
 }
 
 function getPlayList() {
-    let playList = [];
-    
-    let renderer = document.getElementsByClassName('style-scope ytmusic-grid-renderer');
-    for (let i = 0; i < renderer.length; i++) {
-        if (renderer[i].tagName == 'YTMUSIC-TWO-ROW-ITEM-RENDERER') {
-            playList.push(renderer[i]);
-        }
-    }
-
-    return playList;
-}
-function getMix() {
-    let renderer = document.getElementsByClassName('yt-simple-endpoint image-wrapper style-scope ytmusic-two-row-item-renderer');
-    for (let i = 0; i < renderer.length; i++) {
-        let title = renderer[i].getAttribute('title');
-        if (title == '나만의 믹스' || title == 'My Supermix')
-            return renderer[i];
-    }
-    return null;
-}
-function checkExistMix() {
-    return new Promise(async function(resolve) {
-        let bExist = false;
-    
-        if (!checkIsCurrentTab(TAB_HOME)) {
-            let bMoved = await moveTab(TAB_HOME);
-            if (!bMoved) resolve(false);
-        }
-        
-        let mix = getMix();
-        bExist = mix != null;
-        
-        resolve(bExist);
-    });
+    let playListSection = document.getElementById('sections').children[1]
+    return playListSection.children.items.children
 }
 
 // 조작
@@ -98,12 +31,12 @@ function clickNextBtn() {
     nextBtn.click();
 }
 function upVolume() {
-    if (document.hasFocus()) return ;
-    setVolume(getVolume() + 10);
+    let keyEvent = new KeyboardEvent('keydown', {key: 'ArrowRight'})
+    dispatchEvenetToVolumeSlider(keyEvent);
 }
 function downVolume() {
-    if (document.hasFocus()) return ;
-    setVolume(getVolume() - 10);
+    let keyEvent = new KeyboardEvent('keydown', {key: 'ArrowLeft'})
+    dispatchEvenetToVolumeSlider(keyEvent);
 }
 function clickRepeatBtn() {
     let repeatBtn = document.getElementById('expand-repeat');
@@ -113,42 +46,13 @@ function clickShuffleBtn() {
     let shuffleBtn = document.getElementById('expand-shuffle');
     shuffleBtn.click();
 }
-async function startPlayList(playListIndex) {
-    if (!checkIsCurrentTab(TAB_STORAGE)) {
-        let bMoved = await moveTab(TAB_STORAGE);
-        if (!bMoved) return ;
-    }
-    
+function startPlayList(playListIndex) {
     let playList = getPlayList();
     if (playListIndex < 0 || playListIndex >= playList.length)
         return ;
     
-    let playListBtn = playList[playListIndex].getElementsByClassName('icon style-scope ytmusic-play-button-renderer')[0];
+    let playListBtn = playList[playListIndex].getElementsByTagName('ytmusic-play-button-renderer')[0];
     playListBtn.click();
-}
-async function startMix() {
-    if (!checkIsCurrentTab(TAB_HOME)) {
-        let bMoved = await moveTab(TAB_HOME);
-        if (!bMoved) return ;
-    }
-
-    let mix = getMix();
-    let mixBtn = mix.getElementsByClassName('icon style-scope ytmusic-play-button-renderer')[0];
-    mixBtn.click();
-}
-async function startMixIfExist() {
-    let bExistMix = await checkExistMix();
-
-    if (bExistMix)
-        startMix();
-}
-async function startDefault() {
-    let bExistMix = await checkExistMix();
-
-    if (bExistMix)
-        startMix();
-    else
-        startPlayList(1);
 }
 
 function connect() {
@@ -160,7 +64,7 @@ function connect() {
         case "prev":        clickPrevBtn();         break;
         case "next":        clickNextBtn();         break;
         case "start":       startPlayList(msg[1]);  break;
-        case "start_mix":   startMixIfExist();      break;
+        case "start_mix":   startPlayList(0);       break;
         case "volume_down": downVolume();           break;
         case "volume_up":   upVolume();             break;
         case "repeat":      clickRepeatBtn();       break;
@@ -170,7 +74,7 @@ function connect() {
                 if (checkIsPlaying())
                     clickPauseBtn();
                 else
-                    startDefault();
+                    startPlayList(0);
                 break;
             }
         }
